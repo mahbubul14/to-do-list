ba386e51ff50683d52efef72678319fea69548f7
mahbub <mahbub21alam2017@gmail.com>
2021-09-22 21:53:49 +0600

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 // import _ from 'lodash';
 // import './style.css';
-const task =[
+const task = [
   {
     description: 'Adding a new item',
     completed: false,
     index: 0,
   },
   {
-    description: "Going shop",
+    description: 'Going shop',
     completed: false,
     index: 1,
   },
   {
-    description: "Do Exercise",
+    description: 'Do Exercise',
     completed: false,
     index: 2,
   },
@@ -21,23 +21,21 @@ const task =[
 function render() {
   task.forEach((tsk) => {
     const { description } = tsk;
-const taskInput = document.getElementById('task')
-const list = document.getElementById('list')
-const addBtn = document.getElementById('addBtn')
-
-
-  const item = document.createElement('li');
-  const itemInput =document.createElement('input');
-  const itemText = document.createElement('p');
-  itemText.innerHTML = description;
-  const threeDot = document.createElement('i');
-  threeDot.className = "fas fa-ellipsis-v";
-  // itemText.innerHTML = taskInput.value;
-  itemInput.setAttribute("type", "checkbox");
-  list.appendChild(item);
-  item.appendChild(itemInput);
-  item.appendChild(itemText);
-  item.appendChild(threeDot);
-  })
+    // const taskInput = document.getElementById('task');
+    const list = document.getElementById('list');
+    // const addBtn = document.getElementById('addBtn');
+    const item = document.createElement('li');
+    const itemInput = document.createElement('input');
+    const itemText = document.createElement('p');
+    itemText.innerHTML = description;
+    const threeDot = document.createElement('i');
+    threeDot.className = 'fas fa-ellipsis-v';
+    // itemText.innerHTML = taskInput.value;
+    itemInput.setAttribute('type', 'checkbox');
+    list.appendChild(item);
+    item.appendChild(itemInput);
+    item.appendChild(itemText);
+    item.appendChild(threeDot);
+  });
 }
 render();
--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'none',
-    entry: './src/index.js',
-    devServer: {
-      static: './dist',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-          template: './src/index.html'
-        }),
-      ],
-    output: {
+  mode: 'none',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
