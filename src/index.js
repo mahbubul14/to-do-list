97f8028f8d57b784eca155593423f3d64b2aaffe
mahbub <mahbub21alam2017@gmail.com>
2021-09-22 23:28:27 +0600

    Erase inline comments

--- src/index.js
+++ src/index.js
@@ -21,16 +21,13 @@ const task = [
 function render() {
   task.forEach((tsk) => {
     const { description } = tsk;
-    // const taskInput = document.getElementById('task');
     const list = document.getElementById('list');
-    // const addBtn = document.getElementById('addBtn');
     const item = document.createElement('li');
     const itemInput = document.createElement('input');
     const itemText = document.createElement('p');
     itemText.innerHTML = description;
     const threeDot = document.createElement('i');
     threeDot.className = 'fas fa-ellipsis-v';
-    // itemText.innerHTML = taskInput.value;
     itemInput.setAttribute('type', 'checkbox');
     list.appendChild(item);
     item.appendChild(itemInput);
